0430056625a22530cbf4b839e46b66d580e082d7
Aaron Gilmore <aaron@aarongilmore.com>
2017-08-25 21:18:00 +0100

    Website v11 cometh.

--- _site/js/blog.js
+++ _site/js/blog.js
@@ -1,4 +1,4 @@
 $( "p" ).addClass( "lh-copy" );
-$( "a" ).addClass( "link gold" );
+$( "a" ).addClass( "link blue" );
 $( "h3" ).addClass( "f3 lh-title fw3 black-80" );
 $( "ul,ol" ).addClass( "lh-copy" );
