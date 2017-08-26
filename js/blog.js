4b629198decae0372883ab4f2dcf1466790aa25f
Aaron Gilmore <aaron@aarongilmore.com>
2017-08-26 13:17:30 +0100

    Fixing title colours on blog.

--- js/blog.js
+++ js/blog.js
@@ -1,4 +1,4 @@
 $( "p" ).addClass( "lh-copy" );
 $( "a" ).addClass( "link blue" );
-$( "h3" ).addClass( "f3 lh-title fw3 black-80" );
+$( "h3" ).addClass( "f3 lh-title fw3 white" );
 $( "ul,ol" ).addClass( "lh-copy" );
