1fdca84d4b3201dded638f6650684e978a3a6273
Aaron Gilmore <aaron@aarongilmore.com>
2018-03-01 15:50:55 +0000

    Cleaning up files.

--- _site/js/blog.js
+++ _site/js/blog.js
@@ -1,6 +1,6 @@
 $( ".post p" ).addClass( "lh-copy" );
 $( ".post a" ).addClass( "link blue" );
-$( ".post h3" ).addClass( "f3 lh-title fw3 mt4" );
+$( ".post h3" ).addClass( "f3 lh-title fw3 mt5 mb0" );
 $( ".post h4" ).addClass( "f5 gray fw3 mt0" );
 $( ".post li" ).addClass( "lh-list" );
 $( ".post img" ).addClass( "w-100" );
