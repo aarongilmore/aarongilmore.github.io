f080c34ad194ec230e0b52abc16b5a5a8dce154d
Aaron Gilmore <aaron@aarongilmore.com>
2018-08-09 21:20:45 +0100

    New header design

---
+++ _site/js/main.js
@@ -0,0 +1,9 @@
+  document.addEventListener('DOMContentLoaded', function() {
+      var simple = document.querySelector('.js_simple');
+
+      lory(simple, {
+          infinite: 1
+      });
+  });
+
+  $( "nav" ).addClass( "bg-red" );
