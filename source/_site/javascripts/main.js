9eb0de221c1d92f583766c4aebc323755f785ef6
Aaron Gilmore <aaron@aarongilmore.com>
2019-11-12 21:34:24 +0000

    New site.

---
+++ source/_site/javascripts/main.js
@@ -0,0 +1,10 @@
+  document.addEventListener('DOMContentLoaded', function() {
+      var simple = document.querySelector('.js_simple');
+
+      lory(simple, {
+          infinite: 1
+      });
+  });
+
+   	
+
